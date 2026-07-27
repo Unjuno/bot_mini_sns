8f8460657e71342696f065a38f2c28f200b24136
unjuno <unjuno.org@unjuno.org>
2026-07-28 04:21:49 +0900

    Enable all Go platform runtime adapters

--- go/cmd/server/main.go
+++ go/cmd/server/main.go
@@ -80,6 +80,64 @@ func runtimeEvent(payload map[string]any) (common.InboundEvent, replyAdapter, er
 		adapter := common.DiscordAdapter{Token: os.Getenv("DISCORD_BOT_TOKEN")}
 		event, err := adapter.ParseEvent(payload)
 		return event, adapter.SendReply, err
+	case "mastodon":
+		adapter := common.MastodonAdapter{BaseURL: os.Getenv("MASTODON_BASE_URL"), Token: os.Getenv("MASTODON_ACCESS_TOKEN")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "misskey":
+		adapter := common.MisskeyAdapter{BaseURL: os.Getenv("MISSKEY_BASE_URL"), Token: os.Getenv("MISSKEY_TOKEN")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "bluesky":
+		adapter := common.BlueskyAdapter{ServiceURL: os.Getenv("BLUESKY_SERVICE_URL"), JWT: os.Getenv("BLUESKY_ACCESS_JWT"), Repo: os.Getenv("BLUESKY_REPO")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "slack":
+		adapter := common.SlackAdapter{Token: os.Getenv("SLACK_BOT_TOKEN")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "matrix":
+		adapter := common.MatrixAdapter{BaseURL: os.Getenv("MATRIX_BASE_URL"), Token: os.Getenv("MATRIX_ACCESS_TOKEN")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "whatsapp":
+		adapter := common.WhatsAppAdapter{Token: os.Getenv("WHATSAPP_ACCESS_TOKEN"), PhoneNumberID: os.Getenv("WHATSAPP_PHONE_NUMBER_ID")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "viber":
+		adapter := common.ViberAdapter{Token: os.Getenv("VIBER_AUTH_TOKEN")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "zulip":
+		adapter := common.ZulipAdapter{BaseURL: os.Getenv("ZULIP_BASE_URL"), Email: os.Getenv("ZULIP_EMAIL"), APIKey: os.Getenv("ZULIP_API_KEY")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "google_chat":
+		adapter := common.GoogleChatAdapter{Token: os.Getenv("GOOGLE_CHAT_ACCESS_TOKEN")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "teams":
+		adapter := common.TeamsAdapter{Token: os.Getenv("TEAMS_BOT_TOKEN"), ServiceURL: os.Getenv("TEAMS_SERVICE_URL")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "instagram":
+		adapter := common.InstagramAdapter{Token: os.Getenv("INSTAGRAM_ACCESS_TOKEN"), AccountID: os.Getenv("INSTAGRAM_ACCOUNT_ID")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "reddit":
+		adapter := common.RedditAdapter{Token: os.Getenv("REDDIT_ACCESS_TOKEN")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "twitch":
+		adapter := common.TwitchAdapter{Token: os.Getenv("TWITCH_ACCESS_TOKEN"), ClientID: os.Getenv("TWITCH_CLIENT_ID"), BroadcasterID: os.Getenv("TWITCH_BROADCASTER_ID"), SenderID: os.Getenv("TWITCH_SENDER_ID")}
+		event, err := adapter.ParseEvent(payload)
+		return event, adapter.SendReply, err
+	case "kakaotalk":
+		adapter := common.KakaoTalkAdapter{}
+		event, err := adapter.ParseEvent(payload)
+		return event, func(_ common.InboundEvent, reply common.OutboundReply) error {
+			return json.NewEncoder(os.Stdout).Encode(adapter.RenderReply(reply))
+		}, err
 	default:
 		var event common.InboundEvent
 		data, err := json.Marshal(payload)
